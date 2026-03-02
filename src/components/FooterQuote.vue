955709463529c4af6ec6fe4dd474dc90cbc37077
David McKeone <david@artsman.com>
2026-03-02 16:46:18 -0700

    Better FooterQuote

--- src/components/FooterQuote.vue
+++ src/components/FooterQuote.vue
@@ -3,7 +3,7 @@ import Markdown from '@/components/Markdown.vue'
 </script>
 
 <template>
-  <section class="flex-1 relative bg-neutral text-neutral-content px-6 pt-6 pb-12 overflow-hidden">
+  <section class="flex-1 relative bg-neutral text-neutral-content px-6 pt-6 pb-12">
     <blockquote class="relative mx-auto max-w-2xl text-center text-balance">
       <p class="font-serif text-2xl leading-relaxed italic opacity-80">
         <Markdown>"<slot />"</Markdown>
