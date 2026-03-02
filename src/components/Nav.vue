3a6f4c3d4ef666bb5a3680c76a3807c05776f31a
David McKeone <david@artsman.com>
2026-03-02 16:47:25 -0700

    Better nav flex layout

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -5,12 +5,14 @@ const props = defineProps<{ title?: string; subtitle?: string }>()
 </script>
 
 <template>
-  <header class="navbar bg-neutral text-neutral-content shadow-sm print:hidden">
-    <NavBar :title="props.title" :subtitle="props.subtitle" />
-  </header>
-  <main class="flex flex-col fixed h-screen w-full overflow-y-auto bg-base-100 [&>article]:flex [&>article]:flex-col [&>article]:flex-1 [&>article]:min-h-full">
-    <slot></slot>
-  </main>
+  <div class="flex flex-col h-dvh">
+    <header class="navbar bg-neutral text-neutral-content shadow-sm print:hidden">
+      <NavBar :title="props.title" :subtitle="props.subtitle" />
+    </header>
+    <main class="flex flex-col flex-1 overflow-y-auto bg-base-100 [&>article]:flex [&>article]:flex-col [&>article]:flex-1 [&>article]:min-h-full">
+      <slot></slot>
+    </main>
+  </div>
 </template>
 
 <style scoped></style>
