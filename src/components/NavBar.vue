5cd6a2f414cc0b5f575e0226e61453dafd67d56e
David McKeone <david@artsman.com>
2026-03-02 16:50:00 -0700

    Clicking nav menu should now close the menu

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,19 +2,18 @@
 import ThemeToggle from '@/components/ThemeToggle.vue'
 import SocialMedia from '@/components/SocialMedia.vue'
 import { nav } from '@/routes'
-import { computed, ref } from 'vue'
+import { computed, ref, watch } from 'vue'
 import { useRoute } from 'vue-router'
 
 const route = useRoute()
 
 const props = defineProps<{ title?: string; subtitle?: string }>()
 
-const dropdownTrigger = ref<HTMLElement | null>(null)
+const isMenuOpen = ref(false)
 
-function clickMenu() {
-  // Hide menu after clicking on it by blurring the dropdown trigger
-  dropdownTrigger.value?.blur()
-}
+watch(route, () => {
+  isMenuOpen.value = false
+})
 
 const navbarText = computed(() => {
   if (props.title && props.subtitle) {
@@ -53,14 +52,15 @@ const navbarText = computed(() => {
     <div class="inline-block px-2">
       <ThemeToggle />
     </div>
-    <nav class="dropdown dropdown-end">
-      <div
-        ref="dropdownTrigger"
-        tabindex="0"
-        role="button"
+    <div v-if="isMenuOpen" class="fixed inset-0 z-0" aria-hidden="true" @click="isMenuOpen = false" />
+    <nav class="relative z-10">
+      <button
+        type="button"
         aria-label="Open menu"
         aria-haspopup="menu"
+        :aria-expanded="isMenuOpen"
         class="btn btn-ghost btn-circle"
+        @click="isMenuOpen = !isMenuOpen"
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -77,16 +77,16 @@ const navbarText = computed(() => {
             d="M4 6h16M4 12h16M4 18h7"
           />
         </svg>
-      </div>
+      </button>
       <menu
-        tabindex="-1"
-        class="menu menu-md dropdown-content bg-neutral text-neutral-content rounded-box z-1 mt-3 p-2 shadow items-end"
+        v-if="isMenuOpen"
+        class="menu menu-md absolute right-0 top-full bg-neutral text-neutral-content rounded-box mt-3 p-2 shadow [html[data-theme=black]_&]:shadow-white/20 items-end min-w-40"
       >
         <li v-for="{ to, label } in nav" :key="to">
           <RouterLink
-            @click="clickMenu"
             :to="to"
             :aria-current="route.path === to ? 'page' : undefined"
+            @click="isMenuOpen = false"
             >{{ label }}</RouterLink
           >
         </li>
