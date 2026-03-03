d51c0321fe916b473630a1c7836ecc386abf8767
David McKeone <david@artsman.com>
2026-03-03 13:44:39 -0700

    Less whitespace on PageHeader

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -7,15 +7,15 @@ const slots = useSlots()
 </script>
 
 <template>
-  <section class="px-6 py-6 text-center bg-neutral text-neutral-content md:py-12">
+  <section class="px-6 py-6 text-center bg-neutral text-neutral-content">
     <h1 class="font-serif text-4xl font-bold tracking-tight md:text-6xl">{{ title }}</h1>
     <p
       v-if="slots.subtitle"
-      class="font-serif mx-auto mt-1 max-w-lg text-md italic opacity-60 md:mt-2 md:text-xl"
+      class="font-serif mx-auto mt-2 max-w-lg text-md italic opacity-60 md:text-xl"
     >
       <slot name="subtitle" />
     </p>
-    <p v-if="slots.default" class="mx-auto mt-3 max-w-lg text-sm opacity-60 md:mt-4 md:text-lg">
+    <p v-if="slots.default" class="mx-auto mt-3 max-w-lg text-sm opacity-60 md:text-lg">
       <slot />
     </p>
   </section>
