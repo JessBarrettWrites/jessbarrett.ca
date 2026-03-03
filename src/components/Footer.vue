011e540e9dbd200635ba84652cfefa8428e80440
David McKeone <david@artsman.com>
2026-03-03 15:46:55 -0700

    Better footer spacing

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <section class="flex-1 bg-neutral text-neutral-content px-6 py-20">
+  <section class="mt-auto bg-neutral text-neutral-content px-6 py-20">
     <blockquote class="mx-auto max-w-2xl"></blockquote>
   </section>
 </template>
