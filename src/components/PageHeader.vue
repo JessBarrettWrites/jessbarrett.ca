81599a0d5b9622c92fac2e3324ced33a1c6bab50
David McKeone <david@artsman.com>
2026-03-04 08:50:18 -0700

    Increase PageHeader opacity for better readability

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -11,11 +11,11 @@ const slots = useSlots()
     <h1 class="font-serif text-4xl font-bold tracking-tight md:text-6xl">{{ title }}</h1>
     <p
       v-if="slots.subtitle"
-      class="font-serif mx-auto mt-2 max-w-lg text-md italic opacity-60 md:text-xl"
+      class="font-serif mx-auto mt-2 max-w-lg text-md italic opacity-80 md:text-xl"
     >
       <slot name="subtitle" />
     </p>
-    <p v-if="slots.default" class="mx-auto mt-3 max-w-lg text-sm opacity-60 md:text-lg">
+    <p v-if="slots.default" class="mx-auto mt-3 max-w-lg text-sm opacity-80 md:text-lg">
       <slot />
     </p>
   </section>
