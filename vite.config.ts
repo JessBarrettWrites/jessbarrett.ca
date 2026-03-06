07b0eea4bcf9945cb00ffa057f8090558e8992ab
David McKeone <david@prosperos.ca>
2026-03-06 09:19:51 -0700

    Add offline mode with service worker

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,19 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
-import tailwindcss from "@tailwindcss/vite"
+import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    tailwindcss(),
     vue(),
     vueDevTools(),
+    // Tailwind styling for all components
+    tailwindcss(),
+    // Ensure that all images are reasonable sizes at runtime
     ViteImageOptimizer({
       includePublic: true,
       jpg: { quality: 85 },
@@ -18,10 +21,18 @@ export default defineConfig({
       png: { quality: 85 },
       webp: { lossless: false, quality: 85 },
     }),
+    // Use service worker to allow for offline mode
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        // Include all website assets in the pre-cache
+        globPatterns: ['**/*.{js,css,html,ico,jpg,png,svg,woff2}'],
+      },
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
