ada1e6d98607798504d52ff18bf05938d2f75c6b
David McKeone <david@prosperos.ca>
2026-03-12 16:52:36 -0600

    Add github actions test reporter

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,6 @@
 import { fileURLToPath } from 'node:url'
 import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
+import GithubActionsReporter from 'vitest-github-actions-reporter'
 import viteConfig from './vite.config'
 
 export default mergeConfig(
@@ -9,6 +10,7 @@ export default mergeConfig(
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
+      reporters: process.env.GITHUB_ACTIONS ? ['default', new GithubActionsReporter()] : ['default'],
     },
   }),
 )
