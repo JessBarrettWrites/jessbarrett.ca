f102ffc1c0c6da2848f55ef9a1bde4f4fafdbf98
David McKeone <david@prosperos.ca>
2026-07-23 12:54:27 -0700

    Add recommended node version (24) in .nvmrc and typescript and move to native github actions reporter for test results

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,5 @@
 import { fileURLToPath } from 'node:url'
 import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
-import GithubActionsReporter from 'vitest-github-actions-reporter'
 import viteConfig from './vite.config'
 
 export default mergeConfig(
@@ -10,7 +9,7 @@ export default mergeConfig(
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
-      reporters: process.env.GITHUB_ACTIONS ? ['default', new GithubActionsReporter()] : ['default'],
+      reporters: process.env.GITHUB_ACTIONS ? ['default', 'github-actions'] : ['default'],
     },
   }),
 )
